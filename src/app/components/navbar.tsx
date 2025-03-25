b1d85520e870e1993bef92958ff048742b42dc7b
NoNathan17 <nathanong1217@gmail.com>
2025-03-24 22:42:32 -0700

    dynamic navbar that hides when scrolling down

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,11 +1,31 @@
 "use client"
-
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function Navbar() {  
+    const [showNavbar, setNavbar] = useState(true);
+    const [lastScroll, setLastScroll] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScroll = window.scrollY; // returns a px value telling how far user has scrolled
+
+            if (currentScroll < lastScroll) {
+                setNavbar(true); // scrolling up
+            } else {
+                setNavbar(false); // scrolling down
+            }
+
+            setLastScroll(currentScroll); // scrolling
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+        }, [lastScroll]);
     return (
-        /* make navbar so that it comes back when ur scrolling up */
-        <nav className="fixed w-full p-6 md:py-12 md:px-28 text-xl font-stretch-semi-expanded">
+        <nav className={`fixed w-full p-6 md:py-12 md:px-28 text-xl font-stretch-semi-expanded z-50 transition-opacity duration-300 ${
+            showNavbar ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}>
             <div className="flex items-center justify-end gap-7">
                 <Link href="#/" className="duration-300 transition ease-in-out hover:text-blue-300">
                     home
