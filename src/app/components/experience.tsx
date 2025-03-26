f916a77992deae1b29112c9e428fd15ccd1ee96f
NoNathan17 <nathanong1217@gmail.com>
2025-03-25 23:42:16 -0700

    refactor experience section

--- src/app/components/experience.tsx
+++ src/app/components/experience.tsx
@@ -43,6 +43,60 @@ const ExperienceItem: React.FC<ExperienceItemProps> = ({ logo, title, dates, com
 };
 
 export default function Experience() {
+    const experiences = [{
+            logo: "klic.png",
+            altText: "KlicAI Logo",
+            title: "Backend Developer Intern",
+            company: "KlicAI",
+            dates: "Dec. 2024 - Present",
+            description: [
+                'Implemented secure RESTful APIs with Node.js and Express.js to support an innovative dating application',
+                'Managed database schemas with MongoDB, optimizing storage for user profiles, match data, and rewards',
+                'Integrated a token-based reward system and personalized user suggestions for a seamless backend architecture'
+            ],
+            link: "https://klic-ai.com",
+        },
+        {
+            logo: "caretech.jpeg",
+            altText: "CareTech logo",
+            title: "Software Developer",
+            company: "CareTech @ UCI Tech Committee",
+            dates: "Oct. 2024 - Present",
+            description: [
+                'Worked in a team of 8 to develop an automated computer vision system to help patients diagnose melanoma',
+                'Applied image preprocessing techniques with OpenCV to improve the accuracy of skin lesion detection',
+                'Developed the CareTech website, utilizing React, Tailwind CSS, and TypeScript for the frontend design'
+            ],
+            link: "https://caretechuci.vercel.app",
+        },
+        {
+            logo: "UCI.png",
+            altText: "UCI Logo",
+            title: "Python Programming Learning Assistant",
+            company: "KlicAI",
+            dates: "Jan. 2025 - Mar.2025",
+            description: [
+                'Selected as one of 12 Learning Assistants for UCI\'s foundational Object-Oriented Python Programming course',
+                'Facilitated triweekly lab sections for 45+ students, providing guidance through difficult programming concepts',
+                'Conducted one-hour long weekly office hours, addressing unique questions and challenges for 180 students',
+            ],
+            link: "https://ics.uci.edu/",
+        },
+        {
+            logo: "codingmind.png",
+            altText: "Coding Mind Logo",
+            title: "Coding Instructor",
+            company: "Coding Mind Academy",
+            dates: "Oct. 2024 - Mar. 2025",
+            description: [
+                'Educated K-12 students on Python, Scratch, and Roblox Studio, enhancing their computer science skills',
+                'Worked with students through engaging lectures and coding exercises, fostering problem-solving and creativity',
+                'Shaped lesson plans for diverse student needs, exercising patience and creating an inclusive classroom environment'
+            ],
+            link: "https://codingmind.com/",
+        },
+        
+    ]
     return (
         <div id="experience" className="flex min-h-screen">
             <div className="flex flex-col m-auto w-2/3 h-3/5 gap-10">
@@ -69,58 +123,18 @@ export default function Experience() {
                     }}
                     viewport={{once: true, amount: 0.3 }}>
 
-                <div className="flex flex-col gap-5">
-                    <ExperienceItem 
-                        logo="/klic.png"
-                        altText="KlicAI Logo"
-                        title="Backend Developer"
-                        company="KlicAI"
-                        dates="Dec. 2024 - Present"
-                        description={[
-                            'Implemented secure RESTful APIs with Node.js and Express.js to support an innovative dating application',
-                            'Managed database schemas with MongoDB, optimizing storage for user profiles, match data, and rewards',
-                            'Integrated a token-based reward system and personalized user suggestions for a seamless backend architecture',
-                        ]}
-                        link="https://klic-ai.com"/>
+                <div className="flex flex-col gap-5">     
+                    {experiences.map((experience) => (
+                        <ExperienceItem
+                            logo={experience.logo}
+                            altText={experience.altText}
+                            title={experience.title}
+                            company={experience.company}
+                            dates={experience.dates}
+                            description={experience.description}
+                            link={experience.link}/>
+                        ))}   
 
-                    <ExperienceItem 
-                        logo="/caretech.jpeg"
-                        altText="CareTech Logo"
-                        title="Software Developer"
-                        company="CareTech @ UCI Tech Committee"
-                        dates="Oct. 2024 - Present"
-                        description={[
-                            'Worked in a team of 8 to develop an automated computer vision system to help patients diagnose melanoma',
-                            'Applied image preprocessing techniques with OpenCV to improve the accuracy of skin lesion detection',
-                            'Developed the CareTech website, utilizing React, Tailwind CSS, and TypeScript for the frontend design'
-                        ]}
-                        link="https://caretechuci.vercel.app"/>
-
-                    <ExperienceItem 
-                        logo="/UCI.png"
-                        altText="UCI Logo"
-                        title="Python Programming Learning Assistant"
-                        dates="Jan. 2025 - Mar. 2025"
-                        company="Donald Bren School of Information & Computer Sciences"
-                        description={[
-                            'Selected as one of 12 Learning Assistants for UCI\'s foundational Object-Oriented Python Programming course',
-                            'Facilitated triweekly lab sections for 45+ students, providing guidance through difficult programming concepts',
-                            'Conducted one-hour long weekly office hours, addressing unique questions and challenges for 180 students',
-                        ]}
-                        link="https://ics.uci.edu/"/>           
-
-                    <ExperienceItem 
-                        logo="codingmind.png"
-                        altText="Coding Mind Academy Logo"
-                        title="Coding Instructor"
-                        company="Coding Mind Academy"
-                        dates="Oct. 2024 - Mar. 2025"
-                        description={[
-                            'Educated K-12 students on Python, Scratch, and Roblox Studio, enhancing their computer science skills',
-                            'Worked with students through engaging lectures and coding exercises, fostering problem-solving and creativity',
-                            'Shaped lesson plans for diverse student needs, exercising patience and creating an inclusive classroom environment'
-                        ]}
-                        link="https://codingmind.com/"/>
                 </div>
                 </motion.div>
             </div>
