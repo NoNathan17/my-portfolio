e8e7cd18a5f3c1ff14c2a413fb55b4f9dae7d8a9
NoNathan17 <nathanong1217@gmail.com>
2025-03-30 00:40:04 -0700

    updated footer source code link

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -26,7 +26,7 @@ export default function Footer() {
         </ul>
         <div className="invisible lg:visible absolute flex right-5 bottom-3 text-base italic gap-1">
             View Source Code 
-                <Link href="https://github.com/NoNathan17/your-repo" target="_blank" title="View Source Code" className="hover:underline">
+                <Link href="https://github.com/NoNathan17/my-portfolio" target="_blank" title="View Source Code" className="hover:underline">
                     Here
                 </Link>
         </div>
